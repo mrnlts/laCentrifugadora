566e83e276d0a5aeb5e0f2a416316e13cb2cc98a
marina <marinaaltesmagret@gmail.com>
2021-04-21 18:44:47 +0200

    Finito

---
+++ index.js
@@ -0,0 +1,26 @@
+function random(type, input) {
+    const arr = input.split(',');
+    let x;
+    let y;
+
+    switch (type) {
+        case 'people':
+            x = Math.round(Math.random()*5);
+            y = Math.round(Math.random()*5);
+            if (x === y) {
+                x = Math.round(Math.random()*5);
+            }
+            document.getElementById("people").innerText = `${arr[x]}, ${arr[y]}`;
+            break;
+        default:
+            x = Math.round(Math.random()*5);
+            document.getElementById(type).innerText = `${arr[x]}`;
+            break;
+    }
+}
+
+function generaTrama () {
+    random('people', document.getElementById("peopleInput").value);
+    random('place', document.getElementById("placesInput").value);
+    random('problem', document.getElementById("problemsInput").value);
+}
