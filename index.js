2a290ab7ea7e59ba922be0ff9c10d2d2bbbed5d8
marina <marinaaltesmagret@gmail.com>
2021-05-02 11:00:35 +0200

    Versió 2.0

--- index.js
+++ index.js
@@ -1,4 +1,10 @@
-const button = document.getElementById("generaTrama");
+const storyBtn = document.getElementById("generate");
+const lessBtn1 = document.getElementById("less1");
+const lessBtn2 = document.getElementById("less2");
+const lessBtn3 = document.getElementById("less3");
+const moreBtn = document.getElementById("more");
+const downloadBtn = document.getElementById("download");
+let rowCounter = 0;
 
 function random(type, input) {
     const arr = input.split(',');
@@ -25,10 +31,47 @@ function generaTrama () {
     if (!document.getElementById("peopleInput").value || !document.getElementById("placesInput").value || !document.getElementById("problemsInput").value) {
         alert('Et falten camps per omplir!');
     } else {
+    document.getElementById("app").classList.remove("hide");
     random('people', document.getElementById("peopleInput").value);
     random('place', document.getElementById("placesInput").value);
     random('problem', document.getElementById("problemsInput").value);
     }
 }
 
-button.addEventListener('click', generaTrama );
+storyBtn.addEventListener('click', generaTrama );
+
+lessBtn1.addEventListener('click', function deleteField(){
+    console.log(1);
+    document.getElementById("row1").remove();
+    return "finished!"
+});
+
+lessBtn2.addEventListener('click', function deleteField(){
+    console.log(2);
+    document.getElementById("row2").remove();
+    return "finished!"
+});
+
+lessBtn3.addEventListener('click', function deleteField(){
+    console.log(3);
+    document.getElementById("row3").remove();
+    return "finished!"
+});
+
+moreBtn.addEventListener('click', function deleteField(){
+    console.log("more");
+    let node = document.getElementsByTagName("form");
+    const newRow = `<div id="new${rowCounter}"><img src="./img/mes.png" alt="" id="more"><img src="./img/categoria.png" alt=""><label for="newVar"></label><input type="text" id="newVarInput"><img src="./img/llapis2.png" alt=""><input type="text" id="newVarInput"><br></div>`;
+    node.appendChild(newRow);
+    rowCounter++;
+    return "finished!"
+});
+
+downloadBtn.addEventListener('click', function capture(){
+    console.log('download');
+    printJS({    
+        printable: "app",
+        type: "html",
+        css: "./styles.css"
+    })
+});
