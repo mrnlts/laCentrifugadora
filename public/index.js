9540a7c4b5942c6bfc4102dc480f9e8725756e8c
marina <marinaaltesmagret@gmail.com>
2021-06-09 15:18:26 +0200

    Basic form works

--- public/index.js
+++ public/index.js
@@ -1,5 +1,5 @@
 window.addEventListener('load', () => {
-    console.log("loadeasdkjhasdjkhgd!");
+    console.log("loaded!");
 
     const storyBtn = document.getElementById("generate");
     const lessBtn1 = document.getElementById("less1");
@@ -11,25 +11,27 @@ window.addEventListener('load', () => {
     let rowCounter = 0;
     
     function random(type, input, num) {
-        const arr = input.split(',');
-        let result = [];
-        for (let i = 0; i < num; i++) {
-            let elem = Math.round(Math.random()*(arr.length-1));
-            result.push(arr[elem]);
-            arr.splice(elem, 1);
+        if (input) {
+            console.log(type)
+            const arr = input.split(',');
+            let result = [];
+            for (let i = 0; i < num; i++) {
+                let elem = Math.round(Math.random() * (arr.length - 1));
+                result.push(arr[elem]);
+                arr.splice(elem, 1);
+            }
+            let contentTitle;
+            if (type === 'people' || type === 'places' || type === 'problems') {
+                let prevRow = document.getElementById(`${type}Input`);
+                contentTitle = prevRow.parentElement.innerText.split('\n')[0];
+            } else {
+                contentTitle = type.value;
+            }
+            let newRow = document.createElement('li');
+            newRow.innerText = `${contentTitle}: ${result}`
+            let printable = document.getElementById("appResults");
+            return printable.appendChild(newRow);
         }
-        let contentTitle;
-        if (type === 'people' || type === 'places' || type === 'problems') {
-            let prevRow = document.getElementById(`${type}Input`);
-            contentTitle = prevRow.parentElement.innerText.split('\n')[0];
-        } else {
-            contentTitle = type;
-        }
-        let newRow = document.createElement('li');
-        newRow.innerText = `${contentTitle}: ${result}`
-        let printable = document.getElementById("appResults");
-        return printable.appendChild(newRow);
-        // return document.getElementById(type).innerText = `${result}`;
     }
     
     const peopleInput = document.getElementById("peopleInput");
@@ -89,13 +91,8 @@ window.addEventListener('load', () => {
         if (inputs[0].value || inputs[1].value || inputs[2].value) {
             document.getElementById("app").classList.remove("hide");
             variables.forEach(variable => {
-                console.log(variable.input.value)
-                random(variable.type, variable.input, variable.num)
+                random(variable.type, variable.input.value, variable.num.value)
             })
-            // random('people', peopleInput, peopleNum);
-            // random('places', placesInput, placesNum);
-            // random('problems', problemsInput, problemsNum);
-            // (newInput) ? random(newInputTitle, newInput, newInputNum) : '';
             const d = new Date();
             const curr_date = d.getDate();
             const curr_month = d.getMonth() + 1; 
