e491d430f1481c4808c3e37c34a91f21b2a1c80d
marina <marinaaltesmagret@gmail.com>
2021-06-10 18:11:05 +0200

    Modify routes, bug in index.js

---
+++ public/data.js
@@ -0,0 +1,41 @@
+// BLANK FORM ROW CONTENT BY LANGUAGE
+// const casRow = `<img src="/img/mes.png" alt="" id="more"> <img src="/img/categoria.png" alt=""> <input type="text" id="new${rowCounter}Title" name="new${rowCounter}Title" placeholder="Título nueva categoría" class="titleInput"> <img src="/img/llapis2.png" alt=""> <input type="text" id="new${rowCounter}Input" name="new${rowCounter}Input" class="textInput"> <img src="/img/logoqv2.png" alt=""> <input type="text" id="new${rowCounter}Num" name="new${rowCounter}Num" class="numInput"> <br>`;
+// const catRow = `<img src="/img/mes.png" alt="" id="more"> <img src="/img/categoria.png" alt=""> <input type="text" id="new${rowCounter}Title" name="new${rowCounter}Title" placeholder="Títol nova categoria" class="titleInput"> <img src="/img/llapis2.png" alt=""> <input type="text" id="new${rowCounter}Input" name="new${rowCounter}Input" class="textInput"> <img src="/img/logoqv2.png" alt=""> <input type="text" id="new${rowCounter}Num" name="new${rowCounter}Num" class="numInput"> <br>`;
+// const eusRow = `<img src="/img/mes.png" alt="" id="more"> <img src="/img/categoria.png" alt=""> <input type="text" id="new${rowCounter}Title" name="new${rowCounter}Title" placeholder="Kategoriako izenburu berria" class="titleInput"> <img src="/img/llapis2.png" alt=""> <input type="text" id="new${rowCounter}Input" name="new${rowCounter}Input" class="textInput"> <img src="/img/logoqv2.png" alt=""> <input type="text" id="new${rowCounter}Num" name="new${rowCounter}Num" class="numInput"> <br>`;
+// const galRow = `<img src="/img/mes.png" alt="" id="more"> <img src="/img/categoria.png" alt=""> <input type="text" id="new${rowCounter}Title" name="new${rowCounter}Title" placeholder="Título de nova categoría" class="titleInput"> <img src="/img/llapis2.png" alt=""> <input type="text" id="new${rowCounter}Input" name="new${rowCounter}Input" class="textInput"> <img src="/img/logoqv2.png" alt=""> <input type="text" id="new${rowCounter}Num" name="new${rowCounter}Num" class="numInput"> <br`;
+
+// FORM FIELD DOM TARGETTING 
+// const peopleInput = document.getElementById("peopleInput");
+// const peopleNum = document.getElementById("peopleNum");
+// const placesInput = document.getElementById("placesInput");
+// const placesNum = document.getElementById("placesNum");
+// const problemsInput = document.getElementById("problemsInput");
+// const problemsNum = document.getElementById("problemsNum");
+// const newInputTitle = document.getElementById("newVarTitle");
+// const newInput = document.getElementById("newVarInput");
+// const newInputNum = document.getElementById("newVarNum");
+
+// // HISTORY VARIABLES & INPUTS
+// const variables = [
+//     {
+//         type: 'people',
+//         input: peopleInput,
+//         num: peopleNum
+//     },
+//     {
+//         type: 'places',
+//         input: placesInput,
+//         num: placesNum
+//     },
+//     {
+//         type: 'problems',
+//         input: problemsInput,
+//         num: problemsNum
+//     },
+//     {
+//         type: newInputTitle,
+//         input: newInput,    
+//         num: newInputNum
+//     }];
+
+// const inputs = variables.map(elem => elem.input);
