bd12aa13615ea54a71bb00a406960b1a6dadbfbd
marina <marinaaltesmagret@gmail.com>
2021-06-26 20:55:21 +0200

    Adds mobile design for histories pages

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ function random(type, input, num) {
     }
     let contentTitle;
     if (type === 'people' || type === 'places' || type === 'problems') {
-      let prevRow = document.getElementById(`${type}Input`);
+      let prevRow = document.getElementById(`${type}Input`).parentElement;
       contentTitle = prevRow.parentElement.innerText.split('\n')[0];
     } else {
       contentTitle = type.value;
@@ -26,7 +26,7 @@ function random(type, input, num) {
     let newRow = document.createElement('li');
     newRow.innerText = `${contentTitle}: ${result}`;
     let printable = document.getElementById('appResults');
-    return printable.appendChild(newRow);
+    return printable.appendChild(newRow)
   }
 }
 
@@ -75,105 +75,114 @@ function generaTrama() {
   const curr_month = d.getMonth() + 1;
   const curr_year = d.getFullYear();
   document.getElementById('date').innerHTML = curr_date + '-' + curr_month + '-' + curr_year;
+  document.getElementById('generate').parentElement.classList.add('hide');
 }
 
-if (storyBtn) {
-  storyBtn.addEventListener('click', generaTrama);
+storyBtn.addEventListener('click', generaTrama);
 
-  lessBtn1.addEventListener('click', function deleteField() {
-    document.getElementById('row1').remove();
-    let index1 = variables.map(e => e.type === 'people'.indexOf);
-    variables.splice(index1, 1);
-    return 'finished!';
+lessBtn1.addEventListener('click', function deleteField() {
+  document.getElementById('row1').remove();
+  let index1 = variables.map(e => e.type === 'people'.indexOf);
+  variables.splice(index1, 1);
+  return 'finished!';
+});
+
+lessBtn2.addEventListener('click', function deleteField() {
+  document.getElementById('row2').remove();
+  let index2 = variables.map(e => e.type === 'places'.indexOf);
+  variables.splice(index2, 1);
+  return 'finished!';
+});
+
+lessBtn3.addEventListener('click', function deleteField() {
+  document.getElementById('row3').remove();
+  variables.splice(
+    indexof({
+      type: 'problems',
+      input: problemsInput,
+      num: problemsNum,
+    }),
+    1,
+  );
+  return 'finished!';
+});
+
+moreBtn.addEventListener('click', function addField() {
+  let newRow = document.createElement('div');
+  newRow.setAttribute('id', `new${rowCounter}`);
+  let url = window.location.href;
+  let lang = url.substr(url.length - 8, 3);
+  switch (lang) {
+    case 'gal':
+      newRow.innerHTML = `
+        <div>
+        <img src="/img/mes.png" alt="" id="more">
+        <img src="/img/categoria.png" alt="">
+        <input type="text" id="new${rowCounter}Title" name="new${rowCounter}Title" placeholder="Título de nova categoría" class="titleInput">
+        </div>
+        <div>
+        <img src="/img/llapis2.png" alt="">
+        <input type="text" id="new${rowCounter}Input" name="new${rowCounter}Input" class="textInput">
+        <img src="/img/logoqv2.png" alt="">
+        <input type="text" id="new${rowCounter}Num" name="new${rowCounter}Num" class="numInput">
+        </div>`;
+      break;
+    case 'eus':
+      newRow.innerHTML = `
+        <div>              
+        <img src="/img/mes.png" alt="" id="more">
+        <img src="/img/categoria.png" alt="">
+        <input type="text" id="new${rowCounter}Title" name="new${rowCounter}Title" placeholder="Kategoriako izenburu berria" class="titleInput">
+        </div>
+        <img src="/img/llapis2.png" alt="">
+        <input type="text" id="new${rowCounter}Input" name="new${rowCounter}Input" class="textInput">
+        <img src="/img/logoqv2.png" alt="">
+        <input type="text" id="new${rowCounter}Num" name="new${rowCounter}Num" class="numInput">
+        </div>`;
+      break;
+    case 'cat':
+      newRow.innerHTML = `
+        <div>
+        <img src="/img/mes.png" alt="" id="more">
+        <img src="/img/categoria.png" alt="">
+        <input type="text" id="new${rowCounter}Title" name="new${rowCounter}Title" placeholder="Títol nova categoria" class="titleInput">
+        </div>
+        <div>
+        <img src="/img/llapis2.png" alt="">
+        <input type="text" id="new${rowCounter}Input" name="new${rowCounter}Input" class="textInput">
+        <img src="/img/logoqv2.png" alt="">
+        <input type="text" id="new${rowCounter}Num" name="new${rowCounter}Num" class="numInput">
+        </div>`;
+      break;
+    case 'cas':
+      newRow.innerHTML = `
+        <div>              
+        <img src="/img/mes.png" alt="" id="more">
+        <img src="/img/categoria.png" alt="">
+        <input type="text" id="new${rowCounter}Title" name="new${rowCounter}Title" placeholder="Título nueva categoría" class="titleInput">
+        </div>
+        <div>
+        <img src="/img/llapis2.png" alt="">
+        <input type="text" id="new${rowCounter}Input" name="new${rowCounter}Input" class="textInput">
+        <img src="/img/logoqv2.png" alt="">
+        <input type="text" id="new${rowCounter}Num" name="new${rowCounter}Num" class="numInput">
+        </div>`;
+      break;
+  }
+  document.getElementById('form').appendChild(newRow);
+  variables.push({
+    type: document.getElementById(`new${rowCounter}Title`),
+    input: document.getElementById(`new${rowCounter}Input`),
+    num: document.getElementById(`new${rowCounter}Num`),
   });
+  rowCounter++;
+  return 'finished!';
+});
 
-  lessBtn2.addEventListener('click', function deleteField() {
-    document.getElementById('row2').remove();
-    let index2 = variables.map(e => e.type === 'places'.indexOf);
-    variables.splice(index2, 1);
-    return 'finished!';
+downloadBtn.addEventListener('click', function capture() {
+  printJS({
+    printable: 'app',
+    type: 'html',
+    css: '/styles/histories.css',
   });
-
-  lessBtn3.addEventListener('click', function deleteField() {
-    document.getElementById('row3').remove();
-    variables.splice(
-      indexof({
-        type: 'problems',
-        input: problemsInput,
-        num: problemsNum,
-      }),
-      1,
-    );
-    return 'finished!';
-  });
-
-  moreBtn.addEventListener('click', function addField() {
-    let newRow = document.createElement('div');
-    newRow.setAttribute('id', `new${rowCounter}`);
-    let url = window.location.href;
-    let lang = url.substr(url.length - 8, 3);
-    switch (lang) {
-      case 'gal':
-        newRow.innerHTML = `
-                    <img src="/img/mes.png" alt="" id="more">
-                    <img src="/img/categoria.png" alt="">
-                    <input type="text" id="new${rowCounter}Title" name="new${rowCounter}Title" placeholder="Título de nova categoría" class="titleInput">
-                    <img src="/img/llapis2.png" alt="">
-                    <input type="text" id="new${rowCounter}Input" name="new${rowCounter}Input" class="textInput">
-                    <img src="/img/logoqv2.png" alt="">
-                    <input type="text" id="new${rowCounter}Num" name="new${rowCounter}Num" class="numInput">
-                    <br`;
-        break;
-      case 'eus':
-        newRow.innerHTML = `
-                    <img src="/img/mes.png" alt="" id="more">
-                    <img src="/img/categoria.png" alt="">
-                    <input type="text" id="new${rowCounter}Title" name="new${rowCounter}Title" placeholder="Kategoriako izenburu berria" class="titleInput">
-                    <img src="/img/llapis2.png" alt="">
-                    <input type="text" id="new${rowCounter}Input" name="new${rowCounter}Input" class="textInput">
-                    <img src="/img/logoqv2.png" alt="">
-                    <input type="text" id="new${rowCounter}Num" name="new${rowCounter}Num" class="numInput">
-                    <br>`;
-        break;
-      case 'cat':
-        newRow.innerHTML = `
-                    <img src="/img/mes.png" alt="" id="more">
-                    <img src="/img/categoria.png" alt="">
-                    <input type="text" id="new${rowCounter}Title" name="new${rowCounter}Title" placeholder="Títol nova categoria" class="titleInput">
-                    <img src="/img/llapis2.png" alt="">
-                    <input type="text" id="new${rowCounter}Input" name="new${rowCounter}Input" class="textInput">
-                    <img src="/img/logoqv2.png" alt="">
-                    <input type="text" id="new${rowCounter}Num" name="new${rowCounter}Num" class="numInput">
-                    <br>`;
-        break;
-      case 'cas':
-        newRow.innerHTML = `
-                    <img src="/img/mes.png" alt="" id="more">
-                    <img src="/img/categoria.png" alt="">
-                    <input type="text" id="new${rowCounter}Title" name="new${rowCounter}Title" placeholder="Título nueva categoría" class="titleInput">
-                    <img src="/img/llapis2.png" alt="">
-                    <input type="text" id="new${rowCounter}Input" name="new${rowCounter}Input" class="textInput">
-                    <img src="/img/logoqv2.png" alt="">
-                    <input type="text" id="new${rowCounter}Num" name="new${rowCounter}Num" class="numInput">
-                    <br>`;
-        break;
-    }
-    document.getElementById('form').appendChild(newRow);
-    variables.push({
-      type: document.getElementById(`new${rowCounter}Title`),
-      input: document.getElementById(`new${rowCounter}Input`),
-      num: document.getElementById(`new${rowCounter}Num`),
-    });
-    rowCounter++;
-    return 'finished!';
-  });
-
-  downloadBtn.addEventListener('click', function capture() {
-    printJS({
-      printable: 'app',
-      type: 'html',
-      css: '/styles.css',
-    });
-  });
-
-}
+});
